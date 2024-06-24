78b46eb6dff95e5b530bade8b636d2ae809be110
Tsatsu Adogla-Bessa Jnr <tsatsujnr@gmail.com>
2024-06-24 17:15:51 +0000

    add github semantic release plugin (#12)

--- release.config.cjs
+++ release.config.cjs
@@ -8,6 +8,7 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
+    "@semantic-release/github",
     "@semantic-release/npm",
     [
       "@semantic-release/exec",
